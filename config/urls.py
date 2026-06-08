f6cbd341006d15fd6a67398b5d73d1aa2138781c
Braulio Emmanuel Hernandez Martin <braulio.hmartin@alumnos.udg.mx>
2026-06-08 16:51:04 -0600

    arreglar: namespaces de DjangoDebugToolbar en config/ursl.py

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,3 +7,10 @@ urlpatterns = [
     path("api/auth/", include("apps.accounts.urls")),
     path("api/validations/", include("apps.validations.urls")),
 ]
+
+# Debug toolbar solo en desarrollo
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
